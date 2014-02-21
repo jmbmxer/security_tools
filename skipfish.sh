94c06ce09f0d2079f7b89708eed4ca58206b1006
Jimmy Mesta <jmesta@smoke.redspin.com>
2014-02-21 18:06:03 +0000

    deleted dictionary case

--- skipfish.sh
+++ skipfish.sh
@@ -1,36 +1,30 @@
 #bin/sh
-#Define global variables
-REPORT_DIR=/home/my/report/dir
+#Define global variable
+REPORT_DIR=/home/jmesta/mingle
 URI_TO_IGNORE=/css/,/img/,/images/,/js/,/doc/
-TARGET_URL=https://hack.me
-COOKIE=name=value
+TARGET_URL=https://redspin.mingle-staging.thoughtworks.com/
+COOKIE=mingle_13_4_session_id=c498289a30c345bc3b00fda8e77703c4
 TARGET_ROOT_URL=$TARGET_URL
 INTERACTIVE_MODE=YES
-#
+
+
 #Clean up report directory if exists...
-#
+
 if [ -d $REPORT_DIR ];
 then
  rm -rf $REPORT_DIR
 fi
-#
-#Initialize custom dictionary if do not exists...
-#
-if [ ! -f dictionaries/custom-dictionnary.wl ];
-then
- touch dictionaries/custom-dictionnary.wl
-fi
-#
+
 #Define running mode (interactive or quiet)
-#
+
 if [ "$INTERACTIVE_MODE" = "YES" ];
 then
  RUNNING_MODE=""
 else
  RUNNING_MODE="-u"
 fi
-#
+
 #Start scan...
-#
+
 skipfish -b i -I $TARGET_ROOT_URL -X $URI_TO_IGNORE -o $REPORT_DIR -C $COOKIE -M -Q $RUNNING_MODE -G 256 -g 10 -m 10 -f 20 -t 60 -w 60 -i 60 -s 1024000 -e $TARGET_URL
-#
+
